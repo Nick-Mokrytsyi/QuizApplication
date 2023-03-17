2aa96040f8eab787720ac71d8738b866cede024c
nick_mokrytskyi <mykola.mokrytskyi@gmail.com>
2023-03-17 12:50:25 +0100

    Share the project to the git. App accounts is done;

---
+++ README.md
@@ -0,0 +1,52 @@
+# Django QUIZ
+## Technical requirements
+### Web-UI
+  1. Registration
+      - [x] Registration (with email confirmation)
+      - [x] Authorization
+      - [x] Password change
+      - [x] Password reset
+    
+  2. User capabilities
+      - [ ] Passing any test
+      - [ ] Sequentially answering test questions (one after another)
+      - [ ] Finishing a deferred test
+      - [ ] Removing an unfinished test
+      - [ ] Viewing results
+    
+  3. After completing a test
+      - [ ] Report on the number of correct and incorrect answers
+      - [ ] Percentage of correct answers
+
+### Admin site
+  1. [ ] User management
+  2. [ ] Test management
+      - [ ] Adding a test
+      - [ ] Modifying a test
+      - [ ] Deleting a test
+      - [ ] Test validation
+        - [ ] Can't save the question:
+            - [ ] without indicating the correct answer
+            - [ ] where all answers are correct
+        - [ ] Can't save the test if:
+            - [ ] incorrect order_num (should be from 1 to 100 and increase by 1)
+            - [ ] the max value of order_num isn't more than the max allowable number of questions
+            - [ ] the number of questions is less than 3 or more than 100
+
+### Additional requirements
+1. [x] The project must be on Git
+2. [x] requirements.txt file
+3. [x] venv
+4. [ ] PostgresSQL
+5. [ ] The presence of a data dump
+6. [x] Bootstrap
+7. [ ] Unit Tests
+8. [ ] Docker image
+9. [ ] Deployment on Amazon
+10. [ ] Scheduler
+11. [ ] Caching
+
+
+
+ext.: [ ] API + Tests
+
---
+++ src/accounts/views.py
@@ -0,0 +1,87 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.core.signing import BadSignature
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+
+
+from .forms import UserRegisterForm, UserUpdateForm, UserReactivateForm
+from .utils import signer, send_activation_notification
+
+
+class UserRegisterView(CreateView):
+    model = get_user_model()
+    template_name = 'accounts/user_register.html'
+    success_url = reverse_lazy('accounts:register_done')
+    form_class = UserRegisterForm
+
+
+def user_activate(request, sign):
+    try:
+        username = signer.unsign(sign)
+    except BadSignature:
+        return render(request, 'accounts/bad_signature.html')
+
+    user = get_object_or_404(get_user_model(), username=username)
+    if user.is_activated:
+        template = 'accounts/user_is_activated.html'
+    else:
+        template = 'accounts/user_activation_done.html'
+        user.is_activated = True
+        user.is_active = True
+        user.save()
+
+    return render(request, template)
+
+
+def user_reactivate(request):
+    if request.method == "POST":
+        form = UserReactivateForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            User = get_user_model()
+            user = User.objects.filter(email=email).first()
+
+            match user:
+                case None:
+                    return render(request, 'accounts/no_user_in_database.html')
+                case user if user.is_active and user.is_activated:
+                    return render(request, 'accounts/user_is_activated.html')
+                case _:
+                    send_activation_notification(user)
+                    return render(request, 'accounts/user_register_done.html')
+
+        return render(request, 'accounts/user_reactivate.html', {'form': form})
+
+    form = UserReactivateForm()
+    return render(request, 'accounts/user_reactivate.html', {'form': form})
+
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/user_login.html'
+
+
+class UserLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'accounts/user_logout.html'
+
+
+@login_required
+def user_profile_view(request):
+    return render(request, 'accounts/user_profile.html')
+
+
+class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'accounts/user_profile_update.html'
+    model = get_user_model()
+    success_url = reverse_lazy('accounts:profile')
+    form_class = UserUpdateForm
+
+    def get_object(self, queryset=None):
+        return self.request.user
