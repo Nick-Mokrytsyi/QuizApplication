9e1b6d6711faf1be04c8f0c7a016f104dabdfb63
nick_mokrytskyi <mykola.mokrytskyi@gmail.com>
2023-03-20 19:27:46 +0100

    validator for order_numbering --> same question as the maks number of order_num

--- src/quiz/forms.py
+++ src/quiz/forms.py
@@ -13,6 +13,12 @@ class QuestionInlineFormSet(forms.BaseInlineFormSet):
                 f'to {self.instance.QUESTION_MAX_LIMIT} inclusive'
             )
 
+        order_list_num = []
+        for form in self.forms:
+            order_list_num.append(form.cleaned_data['order_num'])  # get the list of all numbers
+        if max(order_list_num) > len(order_list_num):
+            raise ValidationError('You get a mistake in ordering numbers, please check. ***MUST INCREASE BY 1***')
+
 
 class ChoiceInlineFormSet(forms.BaseInlineFormSet):
     def clean(self):
